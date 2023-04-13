2b21e21acec4f8a30cebb127c07fb59b219631ab
qnater <quentin.nater@gmail.com>
2023-04-13 21:01:35 +0200

    Prosody algo

--- app/src/main/java/com/example/checksensoravailability/MainActivity.java
+++ app/src/main/java/com/example/checksensoravailability/MainActivity.java
@@ -10,10 +10,11 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
-import android.hardware.Sensor; //step 4 : import library
+import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
-import android.hardware.SensorManager; //step 4 : import library
+import android.hardware.SensorManager;
+import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
@@ -32,22 +33,31 @@ import androidx.core.content.ContextCompat;
 
 import com.example.checksensoravailability.databinding.ActivityMainBinding;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-
 import java.util.ArrayList;
 import java.util.List;
 
+import be.tarsos.dsp.AudioDispatcher;
+import be.tarsos.dsp.AudioEvent;
+import be.tarsos.dsp.AudioProcessor;
+import be.tarsos.dsp.io.android.AudioDispatcherFactory;
+import be.tarsos.dsp.pitch.PitchDetectionHandler;
+import be.tarsos.dsp.pitch.PitchDetectionResult;
+import be.tarsos.dsp.pitch.PitchProcessor;
+import be.tarsos.dsp.pitch.PitchProcessor.PitchEstimationAlgorithm;
+import be.tarsos.dsp.util.fft.FFT;
 
 
-
-public class MainActivity extends Activity implements SensorEventListener
-{
+public class MainActivity extends Activity implements SensorEventListener {
     private TextView tbxHeartRate;
+
     private ImageView imgView;
     private ImageView imgSphinx;
     private Boolean createFile = true;
@@ -67,6 +77,7 @@ public class MainActivity extends Activity implements SensorEventListener
     private int start = 0;
 
     private Boolean deadLock = false;
+    private int state = 0;
 
     // creating a variable for media recorder object class.
     private MediaRecorder mRecorder;
@@ -76,6 +87,10 @@ public class MainActivity extends Activity implements SensorEventListener
 
     // string variable is created for storing a file name
     private static String mFileName = null;
+    private static String mFileNameSound = null;
+    private static String mFileNameScript = null;
+    private String pitch = "";
+    private String amplitude = "";
 
     // constant for storing audio permission
     public static final int REQUEST_AUDIO_PERMISSION_CODE = 1;
@@ -83,11 +98,8 @@ public class MainActivity extends Activity implements SensorEventListener
     MediaPlayer mediaPlayer;
 
 
-
-
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
@@ -103,25 +115,36 @@ public class MainActivity extends Activity implements SensorEventListener
         checkPermission();
         checkSensorAvailability();
 
-        imgMic.setOnClickListener(new View.OnClickListener()
+
+        extractFeatures();
+
+        imgView.setOnClickListener(new View.OnClickListener()
         {
+           @Override
+           public void onClick(View v)
+           {
+               if(state!=2)
+                   state = state + 1;
+               else
+                   state = 0;
+           }
+        });
+
+
+        imgMic.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 Log.d(TAG, "MODE :" + mode.toString());
 
                 Drawable drawable;
 
-                if(mode)
-                {
+                if (mode) {
                     Log.d(TAG, "START RECORDING___");
                     startRecording();
                     drawable = getDrawable(R.drawable.close_mic);
                     imgMic.setImageDrawable(drawable);
                     mode = false;
-                }
-                else
-                {
+                } else {
                     Log.d(TAG, "STOP RECORDING___");
                     pauseRecording();
                     drawable = getDrawable(R.drawable.open_mic);
@@ -131,11 +154,9 @@ public class MainActivity extends Activity implements SensorEventListener
             }
         });
 
-        imgSphinx.setOnClickListener(new View.OnClickListener()
-        {
+        imgSphinx.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 sphynxCall();
             }
         });
@@ -143,20 +164,16 @@ public class MainActivity extends Activity implements SensorEventListener
     }
 
 
-    private void sphynxCall()
-    {
+    private void sphynxCall() {
         deadLock = false;
 
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "en-US");
 
-        try
-        {
+        try {
             startActivityForResult(intent, RESULT_SPEECH);
-        }
-        catch (ActivityNotFoundException e)
-        {
+        } catch (ActivityNotFoundException e) {
             Toast.makeText(getApplicationContext(), "Your device doesn't support Speach to Text", Toast.LENGTH_LONG).show();
             e.printStackTrace();
         }
@@ -164,40 +181,29 @@ public class MainActivity extends Activity implements SensorEventListener
         deadLock = true;
 
     }
+
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
-    {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        switch (requestCode)
-        {
+        switch (requestCode) {
             case RESULT_SPEECH:
-                if(resultCode == RESULT_OK && data != null)
-                {
+                if (resultCode == RESULT_OK && data != null) {
                     ArrayList<String> text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
 
                     String userCommand = text.get(0);
                     Log.d(TAG, "Sphynx get : " + userCommand);
 
 
-                    if (userCommand.contains("relax"))
-                    {
+                    if (userCommand.contains("relax")) {
                         relaxationMethod(1);
-                    }
-                    else if (userCommand.contains("motivation"))
-                    {
+                    } else if (userCommand.contains("motivation")) {
                         relaxationMethod(2);
-                    }
-                    else if (userCommand.contains("stop"))
-                    {
+                    } else if (userCommand.contains("stop")) {
                         relaxationMethod(0);
-                    }
-                    else if (userCommand.contains("victory"))
-                    {
+                    } else if (userCommand.contains("victory")) {
                         relaxationMethod(3);
-                    }
-                    else
-                    {
+                    } else {
                         Log.d(TAG, "Pattern not understood... Sorry bro...");
                     }
                 }
@@ -206,67 +212,55 @@ public class MainActivity extends Activity implements SensorEventListener
 
     }
 
-    private void relaxationMethod(int played)
-    {
-        switch (played)
-        {
+    private void relaxationMethod(int played) {
+        switch (played) {
             case 1:
-                if(mediaPlayer.isPlaying())
-                {
+                if (mediaPlayer.isPlaying()) {
                     mediaPlayer.stop();
                 }
                 mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.dcl);
                 mediaPlayer.start();
-            break;
+                break;
             case 2:
-                if(mediaPlayer.isPlaying())
-                {
+                if (mediaPlayer.isPlaying()) {
                     mediaPlayer.stop();
                 }
                 mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.op);
                 mediaPlayer.start();
-            break;
+                break;
             case 3:
-                if(mediaPlayer.isPlaying())
-                {
+                if (mediaPlayer.isPlaying()) {
                     mediaPlayer.stop();
                 }
                 mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.bip);
                 mediaPlayer.start();
                 break;
             default:
-                if(mediaPlayer.isPlaying())
-                {
+                if (mediaPlayer.isPlaying()) {
                     mediaPlayer.stop();
                 }
-            break;
+                break;
         }
     }
 
 
-    private void checkPermission()
-    {
+    private void checkPermission() {
         // Runtime permission ------------
-        if (checkSelfPermission(Manifest.permission.BODY_SENSORS) != PackageManager.PERMISSION_GRANTED)
-        {
+        if (checkSelfPermission(Manifest.permission.BODY_SENSORS) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.BODY_SENSORS}, 1);
-        }
-        else
-        {
+        } else {
             Log.d(TAG, "ALREADY GRANTED");
         }
     }
 
 
-    private void checkSensorAvailability()
-    {
+    private void checkSensorAvailability() {
         SensorManager mSensorManager = ((SensorManager) getSystemService(SENSOR_SERVICE));
 
         //List of integrated sensor of device---------
         List<Sensor> sensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
         ArrayList<String> arrayList = new ArrayList<String>();
-        for (Sensor sensor : sensors)
-        {
+        for (Sensor sensor : sensors) {
             arrayList.add(sensor.getName());
         }
         Log.d(TAG, " " + arrayList);
@@ -278,27 +272,49 @@ public class MainActivity extends Activity implements SensorEventListener
 
     public void onSensorChanged(SensorEvent event)
     {
-        if (event.sensor.getType() == Sensor.TYPE_HEART_RATE)
-        {
+
+        if (event.sensor.getType() == Sensor.TYPE_HEART_RATE) {
             int heatBeat = (int) event.values[0];
 
-            if (heatBeat < 80)
-            {
+            if (heatBeat < 80) {
                 Drawable drawable = getDrawable(R.drawable.relax);
+
                 imgView.setImageDrawable(drawable);
+
+
                 tbxHeartRate.setTextColor(Color.BLACK);
+
+                if(state == 1)
+                    tbxHeartRate.setText(pitch + "Hz");
+                else if(state == 2)
+                    tbxHeartRate.setText(amplitude + "Amp");
+
+
             }
             else if (heatBeat > 80 && heatBeat < 100)
             {
                 Drawable drawable = getDrawable(R.drawable.embarassed);
+
                 imgView.setImageDrawable(drawable);
+
                 tbxHeartRate.setTextColor(Color.WHITE);
+
+                if(state == 1)
+                    tbxHeartRate.setText(pitch + " Hz");
+                else if(state == 2)
+                    tbxHeartRate.setText(amplitude + " Amp");
             }
             else
             {
                 Drawable drawable = getDrawable(R.drawable.angry);
                 imgView.setImageDrawable(drawable);
+
                 tbxHeartRate.setTextColor(Color.YELLOW);
+
+                if(state == 1)
+                    tbxHeartRate.setText(pitch + " Hz");
+                else if(state == 2)
+                    tbxHeartRate.setText(amplitude + " Amp");
 
                 Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
                 long[] vibrationPattern = {0, 500, 50, 300};
@@ -306,7 +322,8 @@ public class MainActivity extends Activity implements SensorEventListener
             }
             String msg = "" + heatBeat;
 
-            tbxHeartRate.setText(msg + " bpm");
+            if(state == 0)
+                tbxHeartRate.setText(msg + "bpm");
 
             Log.d(TAG, msg);
 
@@ -315,21 +332,17 @@ public class MainActivity extends Activity implements SensorEventListener
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy)
-    {
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
         Log.d(TAG, "onAccuracyChanged - accuracy: " + accuracy);
     }
 
-    private void writeDataset(int heatBeat)
-    {
-        try
-        {
+    private void writeDataset(int heatBeat) {
+        try {
             File extDir = Environment.getExternalStorageDirectory();
             String filename = "heartbeat_data_set.csv";
             File fullFilename = new File(extDir, filename);
 
-            if (createFile && i == 0)
-            {
+            if (createFile && i == 0) {
                 fullFilename.delete();
                 fullFilename.createNewFile();
                 fullFilename.setWritable(Boolean.TRUE);
@@ -360,13 +373,11 @@ public class MainActivity extends Activity implements SensorEventListener
     }
 
 
-    private void startRecording()
-    {
+    private void startRecording() {
         // check permission method is used to check
         // that the user has granted permission
         // to record and store the audio.
-        if (CheckPermissions())
-        {
+        if (CheckPermissions()) {
             // we are here initializing our filename variable
             // with the path of the recorded audio file.
             mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
@@ -375,7 +386,7 @@ public class MainActivity extends Activity implements SensorEventListener
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss");
             String timestamp = now.format(formatter);
 
-            mFileName += "/"+timestamp+"record.3gp";
+            mFileName += "/" + timestamp + "record.3gp";
 
             // below method is used to initialize
             // the media recorder class
@@ -409,9 +420,7 @@ public class MainActivity extends Activity implements SensorEventListener
 
             mRecorder.start();
 
-        }
-        else
-        {
+        } else {
             // if audio recording permissions are
             // not granted by user below method will
             // ask for runtime permission for mic and storage.
@@ -426,17 +435,17 @@ public class MainActivity extends Activity implements SensorEventListener
         // the audio recording.
         mRecorder.stop();
 
-        // below method will release
         // the media recorder class.
         Log.d(TAG, "Stop recording");
         mRecorder.release();
+
+
         mRecorder = null;
     }
 
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
-    {
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         // this method is called when user will
         // grant the permission for audio recording.
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -455,20 +464,73 @@ public class MainActivity extends Activity implements SensorEventListener
         }
     }
 
-    public boolean CheckPermissions()
-    {
+    public boolean CheckPermissions() {
         // this method is used to check permission
         int result = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
         int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), RECORD_AUDIO);
         return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
     }
 
-    private void RequestPermissions()
-    {
+    private void RequestPermissions() {
         // this method is used to request the
         // permission for audio recording and storage.
         ActivityCompat.requestPermissions(MainActivity.this, new String[]{RECORD_AUDIO, WRITE_EXTERNAL_STORAGE}, REQUEST_AUDIO_PERMISSION_CODE);
     }
 
+    private void extractFeatures()
+    {
+        mFileNameSound = Environment.getExternalStorageDirectory().getAbsolutePath();
+        mFileNameSound += "/" ;
+
+        Log.d("ProsodyFeatures", "mFileNameSound (): " + mFileNameSound);
+
+
+        AudioDispatcher dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050,1024,0);
+        PitchDetectionHandler pdh = new PitchDetectionHandler()
+        {
+            @Override
+            public void handlePitch(PitchDetectionResult res, AudioEvent e)
+            {
+                final float pitchInHz = res.getPitch();
+                final float[] amp = {0};
+                final float[] amplitudes = new float[e.getBufferSize()];
+
+                float[] audioFloatBuffer = e.getFloatBuffer();
+                float[] transformBuffer = new float[e.getBufferSize() * 2];
+                FFT fft = new FFT(e.getBufferSize());
+                System.arraycopy(audioFloatBuffer, 0, transformBuffer, 0, audioFloatBuffer.length);
+                fft.forwardTransform(transformBuffer);
+                fft.modulus(transformBuffer, amplitudes);
+
+
+                for (int index = 0; index < amplitudes.length; index++)
+                {
+                    if (amplitudes[index] > amp[0])
+                    {
+                        amp[0] = amplitudes[index];
+                    }
+                }
+
+
+                pitch = String.valueOf((int) pitchInHz);
+                amplitude= String.valueOf((int) amp[0]);
+
+                runOnUiThread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        System.out.println("ProsodyFeatures : pitchInHz : " + pitchInHz);
+                        System.out.println("ProsodyFeatures : amplitudes : " + amp[0]);
+                    }
+                });
+            }
+        };
+        AudioProcessor pitchProcessor = new PitchProcessor(PitchEstimationAlgorithm.FFT_YIN, 22050, 1024, pdh);
+        dispatcher.addAudioProcessor(pitchProcessor);
+
+        Thread audioThread = new Thread(dispatcher, "Audio Thread");
+        audioThread.start();
+    }
 
 }
